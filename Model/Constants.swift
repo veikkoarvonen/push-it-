8723467e747214f00683488a374f930625a3d587
veikkoarvonen <veikko.arvonen@outlook.com>
2026-01-19 13:50:38 +0200

    Add constants file and custom colors

---
+++ Model/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Push It!
+//
+//  Created by Veikko Arvonen on 19.1.2026.
+//
+
+import Foundation
+
+struct C {
+    
+    struct fonts {
+        static let bold = "SFProDisplay-Bold"
+        static let medium = "SFProDisplay-Medium"
+        static let regular = "SFProDisplay-Regular"
+    }
+    
+    struct colors {
+        static let orange1 = "orange1"
+        static let gray1 = "gray1"
+        static let gray2 = "gray2"
+        static let gray3 = "gray3"
+        static let gray4 = "gray4"
+    }
+    
+    static let testUIwithBackgroundColor: Bool = true
+    
+}
