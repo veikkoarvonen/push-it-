bb9fbc8f84d150e5c88fa9ca4fa0940db94f673d
veikkoarvonen <veikko.arvonen@outlook.com>
2026-01-21 15:27:50 +0200

    Build push ups VC

--- Control/PushUps.swift
+++ Control/PushUps.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class PushUpsVC: UIViewController {
 
+    @IBOutlet weak var cameraLayerView: UIView!
+    @IBOutlet weak var cameraIconView: UIImageView!
+    @IBOutlet weak var pushUpCounterLabel: UILabel!
+    @IBOutlet weak var pushUpsRemainingLabel: UILabel!
+    @IBOutlet weak var pushUpsInstructionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let camTap = UITapGestureRecognizer(target: self, action: #selector(handleCamTap))
+        cameraIconView.addGestureRecognizer(camTap)
+        cameraIconView.isUserInteractionEnabled = true
+    }
+    
+    @objc func handleCamTap() {
+        print("Camera tapped!")
     }
 
 
