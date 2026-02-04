5bdc77c90c2bc4d2fc56d2f8c1107437eddf682f
veikkoarvonen <veikko.arvonen@outlook.com>
2026-02-04 17:57:29 +0200

    Enable only portrait mode

--- Model/AppDelegate.swift
+++ Model/AppDelegate.swift
@@ -11,6 +11,10 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
+    func application(_ application: UIApplication,
+                     supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
